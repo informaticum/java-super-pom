da7976782be8f0ef2a36d958f1e6a3a226d4b062
Stefan Gasterstädt <gasterstaedt@ehealthexperts.de>
2018-03-18 20:29:20 +0100

    Configure the JVM version (default is "1.8")

--- example-projects/jar-project/src/main/java/de/informaticum/jar/HelloWorld.java
+++ example-projects/jar-project/src/main/java/de/informaticum/jar/HelloWorld.java
@@ -1,5 +1,7 @@
 package de.informaticum.jar;
 
+import static java.lang.String.join;
+
 public class HelloWorld {
 
     public static void main(final String[] args) {
@@ -8,7 +10,7 @@ public class HelloWorld {
     }
 
     public String getMessage() {
-        return "Hello world!";
+        return join(" ", "Hello", "world!");
     }
 
 }
